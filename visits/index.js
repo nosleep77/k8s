9f43d07c1d8289cbf29d01631b60cf4319d54485
No Sleep <nosleep@example.com>
2019-04-14 14:13:36 -0400

    adding kubia

--- visits/index.js
+++ visits/index.js
@@ -10,7 +10,7 @@ const client = redis.createClient({
 client.set('visits', 0);
 
 app.get('/', (req, res) => {
-  process.exit(2);
+//  process.exit(2);
   client.get('visits', (err, visits) => {
     res.send('Number of visits is ' + visits);
     client.set('visits', parseInt(visits) + 1);
